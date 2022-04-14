cd9a96a37187d388ac46544524d6e841af52dc38
martincodes <62392843+martincodes-de@users.noreply.github.com>
2022-04-14 20:40:42 +0200

    remove getMealsMethod
    avoid Reihenfolgenabhängigkeit

--- src/Persistence/Services/ScraperService.php
+++ src/Persistence/Services/ScraperService.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace src\Persistence\Services;
 
 use Symfony\Component\DomCrawler\Crawler;
@@ -8,27 +10,27 @@ class ScraperService
 {
     public function __construct(
         private Crawler $crawler,
-        private array $meals = [],
     )
     {
     }
 
-    public function scrapeMainMeals() {
+    public function scrapeMainMeals()
+    {
         $mainMealsSection = $this->crawler->filter("div.splGroupWrapper:nth-child(6)");
         $meals = $mainMealsSection->filter(".splMeal");
+
+        $scrapedMeals = [];
 
         $meals->each(function($node) {
             $mealTitle = $node->filter("div.col-xs-6.col-md-6")->filter("span")->text();
             $mealPrices = $node->filter("div.text-right")->text();
 
-            $this->meals[] = [
+            $scrapedMeals[] = [
                 "title" => $mealTitle,
                 "prices" => $mealPrices
             ];
         });
-    }
 
-    public function getMeals(): array {
-        return $this->meals;
+        return $scrapedMeals;
     }
 }
