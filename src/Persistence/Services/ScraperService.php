0ceb89b2d3adb3a3e7dbfcb3af45aa49e654574a
martincodes <62392843+martincodes-de@users.noreply.github.com>
2022-04-14 19:41:19 +0200

    add default scraper

--- src/Persistence/Services/ScraperService.php
+++ src/Persistence/Services/ScraperService.php
@@ -8,11 +8,20 @@ class ScraperService
 {
     public function __construct(
         private Crawler $crawler,
+        private array $meals = [],
     )
     {
     }
 
     public function scrapeMainMeals(): array {
-        $this->crawler
+        $mainMealsSection = $this->crawler->filter("div.splGroupWrapper:nth-child(6)");
+        $meals = $mainMealsSection->filter(".splMeal");
+
+        $meals->each(function($node) {
+            $mealTitle = $node->filter("div.col-xs-6.col-md-6")->filter("span")->text();
+            $mealPrices = $node->filter("div.text-right")->text();
+
+            $meal = new Meal($mealTitle, PriceConverter::getPriceForStudents($mealPrices));
+        });
     }
 }
