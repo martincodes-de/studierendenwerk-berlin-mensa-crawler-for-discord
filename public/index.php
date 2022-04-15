440bb2500c6e7ff5c6743107d253ebe2b03a8e56
martincodes <62392843+martincodes-de@users.noreply.github.com>
2022-04-15 19:21:56 +0200

    add index file

--- public/index.php
+++ public/index.php
@@ -1,20 +1,37 @@
 <?php
 
 use Goutte\Client;
-use src\Presentation\PriceConverter;
-use src\Presentation\ViewModel\Meal;
+use GuzzleHttp\Client as HttpClient;
+use src\Logic\Converter\MealConverter;
+use src\Logic\Converter\PriceConverter;
+use src\Persistence\ConfigurationDataSource;
+use src\Persistence\MensaHTWTreskowalleeScraper;
+use src\Presentation\ApplicationRunner;
+use src\Presentation\DiscordWebhookSender;
+use src\Presentation\Logger\Logger;
+use src\Presentation\Logger\LogMessageType;
 
-require_once "../vendor/autoload.php";
+require_once __DIR__."/../vendor/autoload.php";
+
+$configuration = new ConfigurationDataSource();
+$logger = new Logger();
 
 $client = new Client();
-$scraper = $client->request("GET", "https://www.stw.berlin/mensen/einrichtungen/hochschule-f%C3%BCr-technik-und-wirtschaft-berlin/mensa-htw-treskowallee.html");
+$crawler = $client->request("GET", $configuration->getScrapedWebsiteUrl());
 
-$mainMealsSection = $scraper->filter("div.splGroupWrapper:nth-child(6)");
-$meals = $mainMealsSection->filter(".splMeal");
+$scraper = new MensaHTWTreskowalleeScraper($crawler);
 
-$meals->each(function($node) {
-    $mealTitle = $node->filter("div.col-xs-6.col-md-6")->filter("span")->text();
-    $mealPrices = $node->filter("div.text-right")->text();
+$httpClient = new HttpClient();
+$priceConverter = new PriceConverter();
+$mealConverter = new MealConverter($priceConverter);
 
-    $meal = new Meal($mealTitle, PriceConverter::getPriceForStudents($mealPrices));
-});
+$discordWebhookSender = new DiscordWebhookSender($configuration->getDiscordWebhookUrl(), $httpClient);
+
+$appRunner = new ApplicationRunner($scraper, $mealConverter, $discordWebhookSender, $configuration, $logger);
+
+try {
+    $appRunner->start();
+} catch (Exception $e) {
+    $logger->write(LogMessageType::ERROR, $e->getMessage());
+    die($e->getMessage());
+}
